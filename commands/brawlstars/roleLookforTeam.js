4356f8a285c9fe6f70c41a53076edd454067f3e2
Abhishek(Icarus) <east7891@gmail.com>
2021-05-08 02:51:53 +0530

    Add role check

--- commands/brawlstars/roleLookforTeam.js
+++ commands/brawlstars/roleLookforTeam.js
@@ -23,12 +23,19 @@ module.exports = class AddRoleLookforTeam extends Command {
         let savedTag = await Database.GetPlayerTag(message.author.id);
         if(savedTag) {
             let role = message.guild.roles.cache.find(r => r.name === "looking-for-team");
-            message.member.roles.add(role).catch(err => console.log(err));
-            console.log(role.color);
-            embed.setColor(`#${(role.color).toString(16)}`);
-            embed.setTitle(`You are assigned "looking-for-team" role`);
-            embed.setDescription('Nice you can now play along with other people!');
-            return await message.author.send(embed);
+            if(message.member.roles.cache.has(role.id)) {
+                embed.setColor('#D40C00');
+                embed.setTitle(`You already have the role ${role.name}`);
+                return await message.embed(embed);
+
+            } else {
+                message.member.roles.add(role).catch(err => console.log(err));
+                embed.setColor(`#${(role.color).toString(16)}`);
+                embed.setTitle(`You are assigned "looking-for-team" role`);
+                embed.setDescription('Nice you can now play along with other people!');
+                return await message.author.send(embed);
+
+            }         
 
 
         } else {
